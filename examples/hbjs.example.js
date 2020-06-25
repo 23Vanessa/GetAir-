1bc039bb327cea197ac62a054e5f60b4b1e8974c
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-06-25 22:06:53 +0000

    add a comment

--- examples/hbjs.example.js
+++ examples/hbjs.example.js
@@ -2,4 +2,5 @@ function example(hb, fontBlob, text) {
   var blob = hb.createBlob(fontBlob);
   var face = hb.createFace(blob, 0);
   // console.log(face.getAxisInfos());
-  var font = hb.createFo
+  var font = hb.createFont(face);
+  // font.se

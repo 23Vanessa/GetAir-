469f815b09172d4d6cb42a5e2bd30aec5671a195
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-06-25 11:15:59 +0000

    Gitignore file

--- examples/hbjs.example.js
+++ examples/hbjs.example.js
@@ -1,4 +1,5 @@
 function example(hb, fontBlob, text) {
   var blob = hb.createBlob(fontBlob);
   var face = hb.createFace(blob, 0);
-  // console.log(face.getAxisInfos(
+  // console.log(face.getAxisInfos());
+  var font = hb.createFo

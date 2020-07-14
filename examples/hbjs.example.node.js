f653f4dfc10f9d1384385d70f3ad99abc83e8fc3
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-07-14 15:14:09 +0000

    Fix code format

--- examples/hbjs.example.node.js
+++ examples/hbjs.example.node.js
@@ -4,4 +4,4 @@ var example = require('./hbjs.example.js');
 
 require('../').then(function (hbjs) {
   console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto.abc.ttf')))));
-  console.log(example(hbjs, new Uint8Array(fs.readFileSyn
+  console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto-Black.

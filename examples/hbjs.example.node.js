998291b47c89348c32a9c1f9ef5adfbfccb637c4
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-07-14 17:58:50 +0000

    Documentation refactor.

--- examples/hbjs.example.node.js
+++ examples/hbjs.example.node.js
@@ -5,4 +5,4 @@ var example = require('./hbjs.example.js');
 require('../').then(function (hbjs) {
   console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto.abc.ttf')))));
   console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto-Black.ttf')))));
-  console.log(examp
+  console.log(example(hbjs, new Uint8Array(fs.r

db45025b994312f11c7f45577a1f47a80448b692
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-07-15 08:13:35 +0000

    fix the readme

--- examples/hbjs.example.node.js
+++ examples/hbjs.example.node.js
@@ -5,4 +5,4 @@ var example = require('./hbjs.example.js');
 require('../').then(function (hbjs) {
   console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto.abc.ttf')))));
   console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Roboto-Black.ttf')))));
-  console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirnam
+  console.log(example(hbjs, new Uint8Array(fs.readFileSync(path.resolve(__dirname, 'Mada.abjad.otf'))), "أب

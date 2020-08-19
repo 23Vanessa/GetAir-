39e1f963890c5f4c0f24ec43c07b6843665787b8
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-08-19 12:08:56 +0000

    Update browser-other.yaml

---
+++ harfbuzz.ts
@@ -0,0 +1,15 @@
+
+﻿type Pointer = number;
+
+const HB_MEMORY_MODE_WRITABLE: number = 2;
+const HB_SET_VALUE_INVALID: Pointer = -1;
+
+class HarfBuzzExports {
+  readonly heapu8: Uint8Array;
+  readonly heapu32: Uint32Array;
+  readonly heapi32: Int32Array;
+  readonly utf8Encoder: TextEncoder;
+
+  //exported HarfBuzz methods
+  readonly malloc: (length: number) => Pointer
+  readonly free: (ptr: Pointer) => void

7b1bc5e0b4b74a5dd74598151c0486187be79b3c
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-08-19 13:41:31 +0000

    release update

--- harfbuzz.ts
+++ harfbuzz.ts
@@ -45,3 +45,40 @@ class HarfBuzzExports {
   constructor(exports: any) {
     this.heapu8 = new Uint8Array(exports.memory.buffer);
     this.heapu32 = new Uint32Array(exports.memory.buffer);
+    this.heapi32 = new Int32Array(exports.memory.buffer);
+    this.utf8Encoder = new TextEncoder();
+
+    this.malloc = exports.malloc;
+    this.free = exports.free;
+    this.free_ptr = exports.free_ptr;
+    this.hb_blob_destroy = exports.hb_blob_destroy;
+    this.hb_blob_create = exports.hb_blob_create;
+    this.hb_face_create = exports.hb_face_create;
+    this.hb_face_get_upem = exports.hb_face_get_upem;
+    this.hb_face_destroy = exports.hb_face_destroy;
+    this.hb_face_collect_unicodes = exports.hb_face_collect_unicodes;
+    this.hb_set_create = exports.hb_set_create;
+    this.hb_set_destroy = exports.hb_set_destroy;
+    this.hb_set_get_population = exports.hb_set_get_population;
+    this.hb_set_next_many = exports.hb_set_next_many;
+    this.hb_font_create = exports.hb_font_create;
+    this.hb_font_set_scale = exports.hb_font_set_scale;
+    this.hb_font_destroy = exports.hb_font_destroy;
+    this.hb_buffer_create = exports.hb_buffer_create;
+    this.hb_buffer_add_utf8 = exports.hb_buffer_add_utf8;
+    this.hb_buffer_guess_segment_properties = exports.hb_buffer_guess_segment_properties;
+    this.hb_buffer_set_direction = exports.hb_buffer_set_direction;
+    this.hb_shape = exports.hb_shape;
+    this.hb_buffer_get_length = exports.hb_buffer_get_length;
+    this.hb_buffer_get_glyph_infos = exports.hb_buffer_get_glyph_infos;
+    this.hb_buffer_get_glyph_positions = exports.hb_buffer_get_glyph_positions;
+    this.hb_buffer_destroy = exports.hb_buffer_destroy;
+  }
+
+}
+
+let hb: HarfBuzzExports;
+
+class CString {
+  readonly ptr: Pointer;
+  readonly length: number;

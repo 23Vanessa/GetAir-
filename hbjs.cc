f9a5d8733418da427037948a802310ca4a6295f2
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-08-28 19:51:43 +0000

    Update database schema

--- hbjs.cc
+++ hbjs.cc
@@ -6,4 +6,5 @@ int
 hbjs_glyph_svg (hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size);
 
 unsigned
-hbjs_shape_with_trace (hb_font_t *fon
+hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
+     

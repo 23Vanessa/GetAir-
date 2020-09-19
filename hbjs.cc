b954d2071bab3b5749123aec0c5ffb4f293175b6
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-09-19 18:12:27 +0000

    Make hard_irq_disable() actually hard-disable interrupts

--- hbjs.cc
+++ hbjs.cc
@@ -90,4 +90,5 @@ line_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
 static void
 quadratic_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
 	      float control_x, float control_y,
-	      float to_
+	      float to_x, float to_y,
+	      void *)

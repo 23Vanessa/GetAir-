75ee6974aafd5a9d3c6901d8556ed7f50767d0fc
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-10-07 09:58:46 +0000

    sctp: Fix race between OOTB responce and route removal

--- hbjs.cc
+++ hbjs.cc
@@ -109,4 +109,5 @@ cubic_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
 {
   _user_data_printf (draw_data, "C%g,%g %g,%g %g,%g",
                      (double)control1_x,
-                     (do
+                     (double)control1_y,
+          

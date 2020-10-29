5f009131baca4744d7311550d059da81e9d68fec
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-10-29 08:50:58 +0000

    Preparing release 1.4

--- hbjs.cc
+++ hbjs.cc
@@ -130,4 +130,4 @@ hbjs_glyph_svg (hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_s
   if (funcs == 0) /* not the best pattern for multi-threaded apps which is not a concern here */
   {
     funcs = hb_draw_funcs_create (); /* will be leaked */
-    hb
+    hb_draw_funcs_set_move_to_func (fun

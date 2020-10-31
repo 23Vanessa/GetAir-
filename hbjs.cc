2524dfb0f8c166520201a3784a420b5b4ca113ff
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2020-10-31 19:07:24 +0000

    Rename CONTRIBUTING to CONTRIBUTING.md

--- hbjs.cc
+++ hbjs.cc
@@ -132,4 +132,5 @@ hbjs_glyph_svg (hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_s
     funcs = hb_draw_funcs_create (); /* will be leaked */
     hb_draw_funcs_set_move_to_func (funcs, (hb_draw_move_to_func_t) move_to, nullptr, nullptr);
     hb_draw_funcs_set_line_to_func (funcs, (hb_draw_line_to_func_t) line_to, nullptr, nullptr);
-    hb_draw_funcs_set_quadratic_to_func (funcs, (hb_draw_quadratic_to_func_t) quadratic_to, nullptr, nu
+    hb_draw_funcs_set_quadratic_to_func (funcs, (hb_draw_quadratic_to_func_t) quadratic_to, nullptr, nullptr);
+    hb_draw_funcs_s

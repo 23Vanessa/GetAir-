4962e8cd7223cb03583805123544115d1e3ac3dd
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-01-12 05:29:00 +0000

    PM / Sleep: Add wake lock api wrapper on top of wakeup sources

--- hbjs.cc
+++ hbjs.cc
@@ -170,4 +170,5 @@ static hb_bool_t do_trace (hb_buffer_t *buffer,
   if (user_data->stop_phase != HB_SHAPE_DONT_STOP) {
     // Do we need to start stopping?
     char buf[12];
-    snprintf (buf, 12, "%d"
+    snprintf (buf, 12, "%d", user_data->stop_at);
+   

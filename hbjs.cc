cce1fcbc21cf605444637744d6a4a8ae4fc86c68
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-04-20 20:30:31 +0000

    Bump to 2.4.0

--- hbjs.cc
+++ hbjs.cc
@@ -200,4 +200,4 @@ hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
                        char* featurestring,
                        unsigned int stop_at, unsigned int stop_phase,
                        char *outbuf, unsigned buf_size) {
-  user_data
+  user_data_t user_data(outbuf, buf_s

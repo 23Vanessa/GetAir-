f11912cabb1bbd8e6571ef829cbc9099fefa5ce2
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-04-20 08:48:33 +0000

    Updating _data/api-commons/new-relic/apis.yaml via Laneworks CMS Publish

--- hbjs.cc
+++ hbjs.cc
@@ -197,4 +197,5 @@ static hb_bool_t do_trace (hb_buffer_t *buffer,
 
 unsigned
 hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
-                       ch
+                       char* featurestring,
+                   

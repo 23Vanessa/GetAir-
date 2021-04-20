9c3c581bd6b063409577d8343dc33f5e77d4de24
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-04-20 19:08:40 +0000

    codestyle fixes

--- hbjs.cc
+++ hbjs.cc
@@ -199,4 +199,5 @@ unsigned
 hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
                        char* featurestring,
                        unsigned int stop_at, unsigned int stop_phase,
-                       char *outbu
+                       char *outbuf, unsigned buf_size) {
+  user_data

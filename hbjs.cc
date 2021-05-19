5f1b8458e568e60c8944dfccd36b0ae0c39a43cf
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-05-19 13:30:22 +0000

    fix about

--- hbjs.cc
+++ hbjs.cc
@@ -229,4 +229,5 @@ hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
   }
 
   hb_buffer_set_message_func (buf, (hb_buffer_message_func_t)do_trace, &user_data, nullptr);
-  user_data.
+  user_data.str[user_data.consumed++] = '[';
+  

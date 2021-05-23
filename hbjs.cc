ecc9b97bb44d3044363cfef6bb7b7cb4fe7e94a1
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-05-23 15:32:18 +0000

    Change indentation

--- hbjs.cc
+++ hbjs.cc
@@ -242,4 +242,4 @@ hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
 #ifdef MAIN
 #include <stdio.h>
 int main() {
-    hb_blob_t *blob = hb_blob_create_from_file ("/home/ebrahim/Desktop/harfbuzzjs/ha
+    hb_blob_t *blob = hb_blob_create_from_file ("/home/ebrahim/Desktop/harfbuzzjs/harfbuzz/test/subset/data/font

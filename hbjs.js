7d9477c8c970cd8a3724e9ae503e6067f2cfa2db
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-06-01 06:15:07 +0000

    Développeur-euse full-stack

--- hbjs.js
+++ hbjs.js
@@ -3,4 +3,5 @@ function hbjs(instance) {
 
   var exports = instance.exports;
   var heapu8 = new Uint8Array(exports.memory.buffer);
-  var heapu32 = new Uint32Array(ex
+  var heapu32 = new Uint32Array(exports.memory.buffer);
+  var

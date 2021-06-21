47bbed51b778482e1f4b3293a4eb3ee23cd4ddd7
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-06-21 07:58:19 +0000

    Updating build-info/dotnet/core-sdk/master

--- hbjs.js
+++ hbjs.js
@@ -44,4 +44,5 @@ function hbjs(instance) {
   **/
   function createBlob(blob) {
     var blobPtr = exports.malloc(blob.byteLength);
-    heapu8.set(new Uint8Array(blob
+    heapu8.set(new Uint8Array(blob), blobPtr);
+    var ptr = exports.hb_

4ebf61c01fc043c711d849a3fe66211a5ec2ab7f
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-06-22 07:27:27 +0000

    Fix clang warnings

--- hbjs.js
+++ hbjs.js
@@ -45,4 +45,4 @@ function hbjs(instance) {
   function createBlob(blob) {
     var blobPtr = exports.malloc(blob.byteLength);
     heapu8.set(new Uint8Array(blob), blobPtr);
-    var ptr = exports.hb_
+    var ptr = exports.hb_blob_create(blobPtr, blob.byteLe

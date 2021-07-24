1b3fb411889c05678d3235bbcc6426de127c2bcd
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-07-24 14:46:41 +0000

    changed README.md

--- hbjs.js
+++ hbjs.js
@@ -112,4 +112,5 @@ function hbjs(instance) {
         var length = exports.hb_blob_get_length(blob);
         if (!length) { return; }
         var blobptr = exports.hb_blob_get_data(blob, null);
-        var table_string = heapu8.subarray(blobptr, blobptr
+        var table_string = heapu8.subarray(blobptr, blobptr+length);
+        return table_str

8d40bfebb78b99a8f985b7e7ef82d8fef19aa7e5
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2021-08-07 06:23:36 +0000

    Add composer.json

--- hbjs.js
+++ hbjs.js
@@ -140,4 +140,4 @@ function hbjs(instance) {
        */
       collectUnicodes: function() {
         var unicodeSetPtr = exports.hb_set_create();
-        exports.hb_face_collect_unicodes(
+        exports.hb_face_collect_unicodes(ptr, unicodeSetPtr);

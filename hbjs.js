e68e9589024f16311d132758f9eb1b7551297a88
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2022-04-17 17:09:17 +0000

    Create server.py

--- hbjs.js
+++ hbjs.js
@@ -345,4 +345,5 @@ function hbjs(instance) {
       /**
       * Return the buffer contents as a JSON object.
       *
-      * After shaping, this function will return an ar
+      * After shaping, this function will return an array of glyph information
+      * objects

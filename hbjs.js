d8fe0d3a4a4077025fa729454cd78631b64b7984
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2022-05-05 08:08:27 +0000

    Revert version

--- hbjs.js
+++ hbjs.js
@@ -353,4 +353,4 @@ function hbjs(instance) {
       *   - ax: Advance width (width to advance after this glyph is painted)
       *   - ay: Advance height (height to advance after this glyph is painted)
       *   - dx: X displacement (adjustment in X dimension when painting this glyph)
-   
+      *   - dy: Y displacement (

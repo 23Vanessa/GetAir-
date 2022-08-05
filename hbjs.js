8d24907accc0950805191197a12d617ec5fa7811
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2022-08-05 10:36:39 +0000

    pythonic

--- hbjs.js
+++ hbjs.js
@@ -401,4 +401,4 @@ function hbjs(instance) {
   /**
   * Shape a buffer with a given font, returning a JSON trace of the shaping process.
   *
-  * This function supports "partial shapi
+  * This function supports "partial shaping", where the shapi

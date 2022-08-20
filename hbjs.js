967acc452b2825b454827a4ea38e07b85905ed3a
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2022-08-20 07:48:47 +0000

    todo update

--- hbjs.js
+++ hbjs.js
@@ -409,4 +409,5 @@ function hbjs(instance) {
   * @param {object} font: A font returned from `createFont`
   * @param {object} buffer: A buffer returned from `createBuffer` and suitably
   *   prepared.
-  * @param {object} features: A dictionary of OpenType feature
+  * @param {object} features: A dictionary of OpenType features to apply.
+  * @param {number} st

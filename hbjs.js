e5ac629815c797a5a153409416502657b7ce7de3
23Vanessa <71664431+23Vanessa@users.noreply.github.com>
2022-08-24 15:00:00 +0000

    thusfar

--- hbjs.js
+++ hbjs.js
@@ -411,4 +411,4 @@ function hbjs(instance) {
   *   prepared.
   * @param {object} features: A dictionary of OpenType features to apply.
   * @param {number} stop_at: A lookup ID at which to terminate shaping.
-  * @param {number} stop_phase: Eit
+  * @param {number} stop_phase: Either 0 (don't terminate shaping), 
